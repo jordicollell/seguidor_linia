0b7f8cd5ebff581fdbf5e02edc8f4da2925fa68d
jordicollell <78138546+jordicollell@users.noreply.github.com>
2021-04-15 08:47:18 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,25 @@
+let marxa = 0
+basic.forever(function () {
+    if (marxa == 0 && input.soundLevel() > 100) {
+        marxa = 1
+    }
+    if (marxa == 1 && input.soundLevel() > 100) {
+        marxa = 0
+    }
+})
 // Seguidor línia central.
 // SENTIT HORARI 
 basic.forever(function () {
-    if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
-        cuteBot.motors(30, 30)
+    while (marxa == 1) {
+        if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+            cuteBot.motors(30, 30)
+        }
+        if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+            cuteBot.motors(10, 30)
+        }
+        if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+            cuteBot.motors(30, 10)
+        }
     }
-    if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
-        cuteBot.motors(10, 30)
-    }
-    if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
-        cuteBot.motors(30, 10)
-    }
+    cuteBot.stopcar()
 })
